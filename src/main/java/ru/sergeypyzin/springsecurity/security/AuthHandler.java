34fe0e226eae14a46fb6ee655667a5761ffa98f8
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-05-01 11:44:02 +0300

    new change AuthHandler

--- src/main/java/ru/sergeypyzin/springsecurity/security/AuthHandler.java
+++ src/main/java/ru/sergeypyzin/springsecurity/security/AuthHandler.java
@@ -3,6 +3,7 @@ package ru.sergeypyzin.springsecurity.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -10,9 +11,10 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import java.io.IOException;
 import java.util.Set;
 
+@Configuration
 public class AuthHandler implements AuthenticationSuccessHandler {
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
