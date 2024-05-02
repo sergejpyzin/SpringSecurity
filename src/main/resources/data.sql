7dc1dd81e7944faead0162e9565391df138a5c1e
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-05-02 16:58:28 +0300

    changed application.properties and created schema.sql and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (product_name, price) VALUES ('Product 1', 10.0);
+INSERT INTO product (product_name, price) VALUES ('Product 2', 20.0);
+INSERT INTO product (product_name, price) VALUES ('Product 3', 30.0);
