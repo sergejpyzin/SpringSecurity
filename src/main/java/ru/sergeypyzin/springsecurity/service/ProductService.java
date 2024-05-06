c054974795785af19c62ce94ca880f5e7ee68e2a
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-05-06 09:39:53 +0300

    added annotation TrackUserAction to ProductService

--- src/main/java/ru/sergeypyzin/springsecurity/service/ProductService.java
+++ src/main/java/ru/sergeypyzin/springsecurity/service/ProductService.java
@@ -2,6 +2,7 @@ package ru.sergeypyzin.springsecurity.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.sergeypyzin.springsecurity.annotation.TrackUserAction;
 import ru.sergeypyzin.springsecurity.model.Product;
 import ru.sergeypyzin.springsecurity.repository.ProductRepository;
 
@@ -17,10 +18,12 @@ public class ProductService {
         return productRepository.findAll();
     }
 
+    @TrackUserAction
     public Product addProduct (Product product) {
         return productRepository.save(product);
     }
 
+    @TrackUserAction
     public String deleteProduct (Long id) {
         productRepository.deleteById(id);
         return "Товар с идентификатором " + id + " был удален";
